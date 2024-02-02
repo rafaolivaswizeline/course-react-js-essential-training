34b088067f93c3c04d197cb9817d508b9b84e139
Rafa Olivas <rafael.olivas@sunpowercorp.com>
2024-02-02 14:48:54 -0700

    Creating controlled form elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import { useRef } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const color = useRef<HTMLInputElement>(null)
-  const text = useRef<HTMLInputElement>(null)
+  const [color, setColor] = useState('#ffffff')
+  const [text, setText] = useState('')
 
   function send() {
-    if (!color.current || !text.current) return
-    alert(`${color.current.value}, ${text.current.value}`)
-    color.current.value = ''
-    text.current.value = ''
+    alert(`${color}, ${text}`)
+    setColor('#ffffff')
+    setText('')
   }
 
   return (
     <>
       <form onSubmit={send}>
-        <input type="text" placeholder="color" ref={text} />
-        <input type="color" ref={color} />
+        <input type="text" placeholder="color" value={text} onChange={(event) => setText(event.target.value)} />
+        <input type="color" value={color} onChange={(event) => setColor(event.target.value)} />
         <button>send</button>
       </form>
     </>
