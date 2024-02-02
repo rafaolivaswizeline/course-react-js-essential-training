89ed7d7b4ebbc1ebc585a2a4469ae547fb093e75
Rafa Olivas <rafael.olivas@sunpowercorp.com>
2024-02-02 14:21:28 -0700

    Incorporating useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useReducer } from 'react'
 import './App.css'
 
 function App() {
-  const [primaryEmotion, setPrimaryEmotion] = useState('happy')
-  const [secondaryEmotion, setSecondaryEmotion] = useState('grateful')
-
-  useEffect(() => {
-    console.log('App component was rendered')
-  }, []) // this empty array makes this hook be executed only on the first render
-
-  useEffect(() => {
-    console.log(`Emotions changed: ${primaryEmotion}, ${secondaryEmotion}`)
-  }, [primaryEmotion, secondaryEmotion]) // when any emotion changes this will be triggered
-
-  useEffect(() => {
-    console.log(`Primary emotion changed: ${primaryEmotion}`)
-  }, [primaryEmotion]) // this will be trigger only when primary emotion changes
-
-  useEffect(() => {
-    console.log(`Secondary emotion changed: ${secondaryEmotion}`)
-  }, [secondaryEmotion]) // this will be trigger only when secondary emotion changes
+  const [checked, setChecked] = useReducer((checked) => !checked, false)
 
   return (
     <>
-      <h1>
-        I'm {primaryEmotion} and {secondaryEmotion}
-      </h1>
-      <section>
-        <button onClick={() => setPrimaryEmotion('happy')}>Happy</button>
-        <button onClick={() => setPrimaryEmotion('sad')}>Sad</button>
-      </section>
-      <section>
-        <button onClick={() => setSecondaryEmotion('grateful')}>Grateful</button>
-        <button onClick={() => setSecondaryEmotion('tired')}>Tired</button>
-      </section>
+      <input id="checkbox" type="checkbox" value={checked.toString()} onChange={setChecked} />
+      <label htmlFor="checkbox">{checked ? 'Checked' : 'Not checked'}</label>
     </>
   )
 }
