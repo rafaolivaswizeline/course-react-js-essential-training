9f12fbf03208fccf15328c44a6e2c33979587f20
Rafa Olivas <rafael.olivas@sunpowercorp.com>
2024-02-02 17:16:07 -0700

    Building a custom Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
 import { useState } from 'react'
 import './App.css'
 
+interface Color {
+  text: string
+  code: string
+}
+
+function useColorInput(color: Color) {
+  const [text, setText] = useState(color.text)
+  const [code, setCode] = useState(color.code)
+
+  function reset() {
+    setText(color.text)
+    setCode(color.code)
+  }
+
+  return [{ text, code }, { setText, setCode }, reset] as [
+    Color,
+    {
+      setText: (value: string) => void
+      setCode: (value: string) => void
+    },
+    () => void,
+  ]
+}
+
 function App() {
-  const [color, setColor] = useState('#ffffff')
-  const [text, setText] = useState('')
+  const [{ text, code }, { setText, setCode }, resetColor] = useColorInput({ text: '', code: '#ffffff' })
 
   function send() {
-    alert(`${color}, ${text}`)
-    setColor('#ffffff')
-    setText('')
+    alert(`${text}, ${code}`)
+    resetColor()
   }
 
   return (
     <>
       <form onSubmit={send}>
         <input type="text" placeholder="color" value={text} onChange={(event) => setText(event.target.value)} />
-        <input type="color" value={color} onChange={(event) => setColor(event.target.value)} />
+        <input type="color" value={code} onChange={(event) => setCode(event.target.value)} />
         <button>send</button>
       </form>
     </>
