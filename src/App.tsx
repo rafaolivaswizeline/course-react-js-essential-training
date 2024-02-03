c7b655737517090b7ba11d8933f95b4657f38f6c
Rafa Olivas <rafael.olivas@sunpowercorp.com>
2024-02-02 22:02:23 -0700

    Fetching data with Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,75 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-interface Color {
-  text: string
-  code: string
+interface ProfileData {
+  login: string
+  created_at: string
+  avatar_url: string
 }
 
-function useColorInput(color: Color) {
-  const [text, setText] = useState(color.text)
-  const [code, setCode] = useState(color.code)
-
-  function reset() {
-    setText(color.text)
-    setCode(color.code)
-  }
-
-  return [{ text, code }, { setText, setCode }, reset] as [
-    Color,
-    {
-      setText: (value: string) => void
-      setCode: (value: string) => void
-    },
-    () => void,
-  ]
+function Profile({ login, created_at, avatar_url }: ProfileData) {
+  return (
+    <>
+      <h1>{login}</h1>
+      <img src={avatar_url} alt={`${login} avatar`} height={150} />
+      <ul>
+        <li>Created at {new Date(created_at).toLocaleString('es-MX')}</li>
+      </ul>
+    </>
+  )
 }
 
 function App() {
-  const [{ text, code }, { setText, setCode }, resetColor] = useColorInput({ text: '', code: '#ffffff' })
+  const [data, setData] = useState<any>('nothing')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<any>(null)
 
-  function send() {
-    alert(`${text}, ${code}`)
-    resetColor()
+  useEffect(() => {
+    setLoading(true)
+
+    function wait(time: number) {
+      return new Promise<void>((resolve) => {
+        setTimeout(() => {
+          resolve()
+        }, time)
+      })
+    }
+
+    async function fetchProfile() {
+      try {
+        await wait(3000)
+        const url = `https://api.github.com/users/rafaolivas19`
+        const data = await (await fetch(url)).json()
+        setData(data)
+        setLoading(false)
+      } catch (error) {
+        setError(error)
+      }
+    }
+
+    fetchProfile()
+  }, [])
+
+  if (error) {
+    return (
+      <>
+        <h1>Error</h1>
+        <pre>{error.toString()}</pre>
+      </>
+    )
+  }
+
+  if (loading) {
+    return (
+      <>
+        <h1>Wait for it...</h1>
+      </>
+    )
   }
 
   return (
     <>
-      <form onSubmit={send}>
-        <input type="text" placeholder="color" value={text} onChange={(event) => setText(event.target.value)} />
-        <input type="color" value={code} onChange={(event) => setCode(event.target.value)} />
-        <button>send</button>
-      </form>
+      <Profile {...data}></Profile>
     </>
   )
 }
