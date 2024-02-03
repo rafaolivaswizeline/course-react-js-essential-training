4219560959dc798964307ffbdb7f3161623fb2f8
Rafa Olivas <rafael.olivas@sunpowercorp.com>
2024-02-02 22:25:46 -0700

    Fetching data with GraphQL

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-interface ProfileData {
-  login: string
-  created_at: string
-  avatar_url: string
+interface LiftData {
+  name: string
+  status: string
 }
 
-function Profile({ login, created_at, avatar_url }: ProfileData) {
-  return (
-    <>
-      <h1>{login}</h1>
-      <img src={avatar_url} alt={`${login} avatar`} height={150} />
-      <ul>
-        <li>Created at {new Date(created_at).toLocaleString('es-MX')}</li>
-      </ul>
-    </>
-  )
+function Lift({ name, status }: LiftData) {
+  return <>{`${name}: ${status}`}</>
 }
 
 function App() {
-  const [data, setData] = useState<any>('nothing')
+  const [data, setData] = useState<any>(null)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<any>(null)
 
@@ -38,9 +29,26 @@ function App() {
     async function fetchProfile() {
       try {
         await wait(3000)
-        const url = `https://api.github.com/users/rafaolivas19`
-        const data = await (await fetch(url)).json()
-        setData(data)
+        const url = `https://snowtooth.moonhighway.com/`
+
+        const options: RequestInit = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            query: `query {
+              allLifts {
+                name
+                status
+              }
+            }`,
+          }),
+        }
+
+        const data = await (await fetch(url, options)).json()
+        console.log(data.data)
+        setData(data.data)
         setLoading(false)
       } catch (error) {
         setError(error)
@@ -69,7 +77,14 @@ function App() {
 
   return (
     <>
-      <Profile {...data}></Profile>
+      <h1>Lifts</h1>
+      <ul>
+        {data?.allLifts?.map((lift: LiftData) => (
+          <li>
+            <Lift {...lift}></Lift>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
